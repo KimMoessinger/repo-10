da2e5b39abde2ceac7b5804d51ed37c612e24e0a
KimMoessinger <KimMoessinger@stud.hs-mannheim.de>
2015-09-28 14:22:31 +0200

    Erster Upload

---
+++ exercise1/src/Polynom.java
@@ -0,0 +1,165 @@
+/**
+ * Diese Klasse präsentiert ein Polynom von einem beliebigen Grad mit
+ * realen Koeffizienten.
+ * <p>
+ * Polynome können addiert und subtrahiert werden. Außerdem ist es möglich,
+ * die Koeffizienten einzeln oder en-bloc wieder auszulesen. Für beliebige
+ * Werte x kann der Polynom ausgerechnet werden.
+ * <p>
+ * Es besteht die Möglichkeit das Polynom zu differnzieren oder zu
+ * integrieren.
+ * <p>
+ * Objekte dieser Klasse sind immutable. Die Methoden verändern den Zustand
+ * des Objektes nicht sondern geben grundsätzlich ein neues Objekt zurück.
+ * <p>
+ * Die Indices beim Zugriff auf die Koeffizienten beginnen immer
+ * bei {@literal 0}
+ */
+public class Polynom {
+
+    /*
+     * Hilfe, ich bin werde auf der Mother Base gefangen gehalten
+     * und muss hier den ganzen Tag Programme schreiben...
+     *
+     * Ich lösche jetzt einfach die meisten Implementierung der
+     * Methoden und flüchte mit dem Schlauchboot, vielleicht finden
+     * sie mich ja nicht....
+     */
+
+    /**
+     * Erzeugt ein neues Polynom mit den gegebenen Koeffizienten.
+     * Die Koeffizienten beginnen Element a0 und setzen sich dann zu
+     * höheren Koeffizienten fort. D.h., dass z.B. das Polynom
+     * <code>2x^4 - 1x^3 + 0,5x^2</code> erzeugt werden muss als:
+     * <code>new Polynom(0.0, 0.0, 0.5, -1.0, 2.0)</code>.
+     *
+     * @param a Koeffizienten des Polynoms
+     */
+    public Polynom(double... a) {
+    }
+
+    /**
+     * Erzeugt das Nullpolynom, d.h. das Polynom, bei dem alle Koeffizienten
+     * 0 sind.
+     */
+    public Polynom() {
+    }
+
+    /**
+     * Berechnet das Polynom an der Stelle x.
+     *
+     * @param x Stelle, für die das Polynom berechnet werden soll.
+     * @return Wert an der Stelle x
+     */
+    public double berechne(double x) {
+        return 0.0;
+    }
+
+    /**
+     * Berechnet das Polynom an mehreren Stellen.
+     *
+     * @param xs die Stellen an der das Polynom berechnet werden soll.
+     * @return Ergebnis
+     */
+    public double[] calc(double[] xs) {
+        return null;
+    }
+
+    /**
+     * Liefert den Grad des Polynoms.
+     *
+     * @return Grad des Polynoms, -1 für das Nullpolynom
+     */
+    public int getGrad() {
+        return -1;    }
+
+    /**
+     * Liefert den n-ten Koeffizienten.
+     *
+     * @param n Koeffizient, der gelesen werden soll.
+     * @return Wert des Koeffizienten oder 0 wenn n größer als der Grad des
+     *         Polynoms ist
+     */
+    public double getKoeffizient(int n) {
+        return 0;
+    }
+
+    /**
+     * Liefert die Koeffizienten dieses Polynoms.
+     *
+     * @return die Koeffizienten.
+     */
+    public double[] getKoeffizienten() {
+        return null;
+    }
+
+    /**
+     * Addiert das gegebenen Polynom zu diesem.
+     *
+     * @param p Polynom, das addiert werden soll.
+     * @return neues Polynom mit dem Ergebnis der Addition
+     */
+    public Polynom addiere(Polynom p) {
+        return null;
+    }
+
+    /**
+     * Subtrahiert das gegebenen Polynom von diesem.
+     *
+     * @param p Polynom, das subtrahiert werden soll.
+     * @return neues Polynom mit dem Ergebnis der Subtraktion
+     */
+    public Polynom subtrahiere(Polynom p) {
+        return null;
+    }
+
+    /**
+     * Differenziert das Polynom.
+     *
+     * @return Ergebnis der Ableitung
+     */
+    public Polynom differenziere() {
+        return null;
+    }
+
+    /**
+     * Integriert das Polynom.
+     *
+     * @return Ergebnis der Integration.
+     */
+    public Polynom integriere() {
+        return null;
+    }
+
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    /**
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+
+    /**
+     * Diese Methode wandelt das Polynom in einen lesbaren String um.
+     * Dabei lässt sie alle Koeffizienten aus, die 0 sind und gibt den
+     * Grad vorweg aus. Für das Polynom -9x^8 - 4x^5 - 0,5x^4 + 5x^2 - 2x + 3
+     * lautet die Ausgabe:
+     * <code>
+     * Grad 8: -9x^8 - 4x^5 - 0,5x^4 + 5x^2 - 2x + 3
+     * </code>
+     *
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "";
+    }
+}
---
+++ exercise1/src/TestPolynom.java
@@ -0,0 +1,140 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests für die Klasse Polynom.
+ */
+public class TestPolynom {
+
+    private static final double PRECISION = 0.0001;
+
+    @Test
+    public void testCreation() {
+        Polynom p1 = new Polynom();
+        assertEquals(-1, p1.getGrad());
+
+        p1 = new Polynom(0, 1, 0);
+        assertEquals(1, p1.getGrad());
+
+        p1 = new Polynom(0, 1, 0, 1, 0, 1, 0, 0, 0);
+        assertEquals(5, p1.getGrad());
+
+        p1 = new Polynom(1.5);
+        assertEquals(0, p1.getGrad());
+
+        double data[] = { 4, 3, 2, 1, 0, -1, -2, -3, -4 };
+        p1 = new Polynom(data);
+        assertArrayEquals(data, p1.getKoeffizienten(), PRECISION);
+
+        p1 = new Polynom(data);
+        assertEquals(4, p1.getKoeffizient(0), PRECISION);
+        assertEquals(3, p1.getKoeffizient(1), PRECISION);
+        assertEquals(-1, p1.getKoeffizient(5), PRECISION);
+    }
+
+    @Test
+    public void testBerechnung() {
+        Polynom p = new Polynom(5);
+
+        assertEquals(5.0, p.berechne(1.0), PRECISION);
+        assertEquals(5.0, p.berechne(4.0), PRECISION);
+        assertEquals(5.0, p.berechne(-1.0), PRECISION);
+
+        p = new Polynom(-1, 5);
+
+        assertEquals(-1.0, p.berechne(0.0), PRECISION);
+        assertEquals(4.0, p.berechne(1.0), PRECISION);
+        assertEquals(-6.0, p.berechne(-1.0), PRECISION);
+
+        p = new Polynom(5, 0, -2,  0, 9.5, -8, 5);
+        assertEquals(9.5, p.berechne(1), PRECISION);
+        assertEquals(25.5, p.berechne(-1), PRECISION);
+        assertEquals(59017.5, p.berechne(5), PRECISION);
+        assertEquals(109017.5, p.berechne(-5), PRECISION);
+    }
+
+    @Test
+    public void testAddition() {
+        Polynom p1 = new Polynom(5, 4, -2,  3, 9.5, -8);
+        Polynom p2 = new Polynom(3, 7,  6, -6, 0.0,  4, 7, 11);
+        Polynom pr = new Polynom(8, 11, 4, -3, 9.5, -4, 7, 11 );
+
+
+        assertEquals(pr, p1.addiere(p2));
+
+
+        p1 = new Polynom(5, 4, -2,  3, 9.5, -8);
+        p2 = new Polynom(3, 7,  6, -6, 0.0);
+        pr = new Polynom(8, 11, 4, -3, 9.5, -8);
+
+        assertEquals(pr, p1.addiere(p2));
+
+        p1 = new Polynom(5, 4, -2,  3, 9.5, -8);
+        p2 = new Polynom();
+
+        assertEquals(p1, p1.addiere(p2));
+
+
+        p1 = new Polynom();
+        p2 = new Polynom(5, 4, -2,  3, 9.5, -8);
+
+        assertEquals(p2, p1.addiere(p2));
+    }
+
+    @Test
+    public void testSubtraction() {
+        Polynom p1 = new Polynom(5, 4, -2,  3, 9.5, -8);
+        Polynom p2 = new Polynom(3, 7,  6, -6, 0.0,  4, 7, 11);
+        Polynom pr = new Polynom(2, -3, -8, 9, 9.5, -12, -7, -11);
+
+        assertEquals(pr, p1.subtrahiere(p2));
+
+        p1 = new Polynom(5, 4, -2,  3, 9.5, -8);
+        p2 = new Polynom(3, 7,  6, -6, 0.0);
+        pr = new Polynom(2, -3, -8, 9, 9.5, -8 );
+
+        assertEquals(pr, p1.subtrahiere(p2));
+
+
+        p1 = new Polynom(5, 4, -2,  3, 9.5, -8);
+        p2 = new Polynom();
+
+        assertEquals(p1, p1.subtrahiere(p2));
+
+
+        p1 = new Polynom();
+        p2 = new Polynom(5, 4, -2,  3, 9.5, -8);
+        pr = new Polynom(-5, -4, 2,  -3, -9.5, 8 );
+
+        assertEquals(pr, p1.subtrahiere(p2));
+    }
+
+    @Test
+    public void testDifferenzierenUndIntegrieren() {
+        Polynom p1 = new Polynom(-10, 0, 0.5, -1, 2);
+        Polynom pr = new Polynom(0, 1, -3, 8);
+
+        System.out.println(p1);
+        System.out.println(pr);
+
+        assertEquals(pr,  p1.differenziere());
+
+        Polynom pi = new Polynom(0, 0, 0.5, -1, 2);
+        assertEquals(pi,  pr.integriere());
+    }
+
+    @Test
+    public void testEquals() {
+        assertEquals(new Polynom(), new Polynom());
+        assertEquals(new Polynom(), new Polynom(0));
+        assertEquals(new Polynom(0,0,1,0,0), new Polynom(0,0,1));
+        assertEquals(new Polynom(1, 2, 3, 4), new Polynom(1, 2, 3, 4));
+        assertFalse(new Polynom().equals(new Polynom(1)));
+    }
+
+    @Test
+    public void testToString() {
+        Polynom p = new Polynom(3, -2, 5, 0, -0.5, -4, 0, 0, -9);
+        assertEquals("Grad 8:  -9x^8 - 4x^5 - 0,5x^4 + 5x^2 - 2x + 3", p.toString());
+    }
+}
