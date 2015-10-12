c12ee2899700aaccb7c38e0d0b3842493acc731a
KimMoessinger <KimMoessinger@stud.hs-mannheim.de>
2015-10-12 14:19:00 +0200

    Dokumentation 1.0

--- exercise2/src/de/hs_mannheim/tpe/gruppe10/exercise2/Figur.java
+++ exercise2/src/de/hs_mannheim/tpe/gruppe10/exercise2/Figur.java
@@ -1,16 +1,48 @@
 package de.hs_mannheim.tpe.gruppe10.exercise2;
 
+/**
+ * 
+ * Klasse Figur
+ * 
+ * Diese Klasse gibt die Fläche von unterschiedlichen Figuren zurück. 
+ * <p>
+ * Es sind die Klassen Dreieck, Gerade, Quadrat und Rechteck zusätzlich vorhanden.
+ * <p>
+ * Eine eigene Unit-Tests ist ebenfalls lauffähig vorhanden.
+ * <p>
+ * Objekte dieser Klasse können nur von Subklassen bearbeitet werden, sofern die
+ * Subklassen in der Vererbung dieser Klasse aufgeführt sind.
+ * Andere Klassen, welche nicht in der Vererbung dieser Klasse aufgeführt sind können
+ * jedoch lesend auf die Objekte zugreifen.
+ * <p>
+ * @author Tobias Jünemann und Kim Mössinger
+ * @version 1.0
+ */
+
 public class Figur {
 	
 	private double flaeche;
+	
+	/**
+     * Erzeugt eine neue Fläche mit den übergebenen Werten.
+     * @param Fläche der einzelnen Figuren.
+     */
     
     protected Figur(double flaeche) {
     	this.flaeche = flaeche;
     }
     
+    /**
+     * Erzeugt die Fläche mit einem Wert 0.
+     */
+    
     protected Figur() {
     	this.flaeche = 0;
     }
+    
+    /**
+     * Gibt Fläche aus.
+     */
     
     public double getFlaeche() {
     	return this.flaeche;
